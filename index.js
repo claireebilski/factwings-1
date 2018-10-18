e8b28f8f9830133d7994d12452a0e3d20e6a11d6
claireebilski <35465034+claireebilski@users.noreply.github.com>
2018-10-18 14:59:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express = require('express')
+//var express = require('express')
 var app = express()
-var cors = require('cors')
-var mongo = require('mongodb');
-var monk = require('monk');
+//var cors = require('cors')
+//var mongo = require('mongodb');
+//var monk = require('monk');
 var db = monk('localhost:27017/factwings');
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
     req.db = db;
-    next();
 });
 
 
@@ -19,14 +18,14 @@ app.get('/factbook/:id', function(req, res) {
     });
 })
 
-app.get('/restaurants/v1/burgers/:city', cors(), function(req, res) {
-    var db = req.db;
-    var collection = db.get('restaurants');
-    collection.find({ "type": "burger", "branches.city": req.params.city }, {}, function(e, docs) {
-        res.send(docs);
-    });
-})
-
+//app.get('/restaurants/v1/burgers/:city', cors(), function(req, res) {
+  //  var db = req.db;
+    //var collection = db.get('restaurants');
+    //collection.find({ "type": "burger", "branches.city": req.params.city }, {}, function(e, docs) {
+      //  res.send(docs);
+   // });
+//})
+app.set('json spaces', 2);
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
